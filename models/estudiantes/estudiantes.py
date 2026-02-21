3cf7c7f156396434578fdc93b46f6d0ac7b45961
Danko <chevesichdanko@gmail.com>
2026-02-21 12:06:23 -0300

    Se crea crud basico, luego se agregaran mas validaciones

--- models/estudiantes/estudiantes.py
+++ models/estudiantes/estudiantes.py
@@ -17,3 +17,54 @@ class Estudiantes:
             WHERE idestudiante = %s
             """
         return database.ejecutar_query(q, (idestudiante,))
+
+    @staticmethod
+    def existe_email(email):
+        q = """
+            SELECT 1 FROM estudiantes
+            WHERE email = %s
+            LIMIT 1
+            """
+        result = database.ejecutar_query(q, (email,))
+        return bool(result)
+
+    @staticmethod
+    def post_estudiantes(data):
+        q = """
+            INSERT INTO estudiantes (nombre, email, telefono)
+            VALUES (%s, %s, %s)
+            RETURNING idestudiante
+            """
+        prms = (
+            data.get("nombre"),
+            data.get("email"),
+            data.get("telefono")
+        )
+        return database.ejecutar_query(q, prms)
+
+    @staticmethod
+    def delete_estudiantes(idestudiante):
+        q = """
+            DELETE FROM estudiantes 
+            WHERE idestudiante = %s;
+            """
+
+        return database.ejecutar_query(q, (idestudiante,), fetch=False)
+
+    @staticmethod
+    def actualizar_estudiantes(data, idestudiante):
+        q = """
+            UPDATE estudiantes
+            SET nombre = %s,
+            email = %s,
+            telefono = %s
+            WHERE idestudiante = %s
+            """
+        prms = (
+            data.get("nombre"),
+            data.get("email"),
+            data.get("telefono"),
+            idestudiante,
+        )
+
+        return database.ejecutar_query(q, prms, fetch=False)
--- nspace/estudiantes/estudiantes.py
+++ nspace/estudiantes/estudiantes.py
@@ -14,6 +14,26 @@ class EstudianteGet(Resource):
             return [], 200
         return response, 200
 
+    def post(self):
+        data = name_space.payload or {}
+
+        nombre = data.get("nombre")
+        email = data.get("email")
+
+        if not nombre or not email:
+            return {"message": "nombre y email son obligatorios"}, 400
+
+        if estudiantes.Estudiantes.existe_email(email):
+            return {"message": "El email ya está registrado"}, 409
+
+        response = estudiantes.Estudiantes.post_estudiantes(data)
+        if not response:
+            return {"message": "Error al crear un estudiante"}, 500
+        return {
+            "message": "Estudiante creado correctamente",
+            "idestudiante": response[0]["idestudiante"]
+        }, 201
+
 
 @name_space.route("/<int:idestudiante>")
 class EstudianteGetId(Resource):
@@ -22,3 +42,26 @@ class EstudianteGetId(Resource):
         if not response:
             return {"message": "Estudiante no encontrado"}, 404
         return response[0], 200
+
+    def delete(self, idestudiante):
+        response = estudiantes.Estudiantes.delete_estudiantes(idestudiante)
+        if not response:
+            return {"message": "Estudiante no encontrado con el id proporcionado"}, 404
+        return {
+            "message": "Estudiante eliminado."}, 200
+
+    def put(self, idestudiante):
+        data = name_space.payload or {}
+
+        nombre = data.get("nombre")
+        email = data.get("email")
+
+        if not nombre or not email:
+            return {"message": "nombre y email son obligatorios"}, 400
+
+        response = estudiantes.Estudiantes.actualizar_estudiantes(
+            data, idestudiante)
+
+        if not response:
+            return {"message": "Error al actualizar estudiante"}, 400
+        return {"message": "Estudiante actualizado"}, 200
